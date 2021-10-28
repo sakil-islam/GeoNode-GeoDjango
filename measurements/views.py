ba6bb68c662117294c69f890549109e7202299c8
Mehedi Hasan <mehedi.cse14bu@gmail.com>
2021-10-28 12:46:14 +0600

    location and Destination distance calculation

--- measurements/views.py
+++ measurements/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Measurement
 from .forms import MeasurementModelForm
 from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+from .utils import get_geo
 # Create your views here.
 
 
@@ -10,16 +12,35 @@ def calculate_distance_view(request):
     form = MeasurementModelForm(request.POST or None)
     geolocator = Nominatim(user_agent='measurements')
 
+    ip = '72.14.207.99'
+    country, city, lat, lon = get_geo(ip)
+    location = geolocator.geocode(city)
+
+    # location coordinates
+
+    l_lat = lat
+    l_lon = lon
+    pointA = (l_lat, l_lon)
+
     if form.is_valid():
         instance = form.save(commit=False)
         destination_ = form.cleaned_data['destination']
         destination = geolocator.geocode(destination_)
-        print(destination)
-        print(destination.latitude)
-        print(destination.longitude)
-        instance.location = 'San Francisco'
-        instance.distance = 5000.00
-        # instance.save()
+
+        # Destination Coordinates
+
+        d_lat = destination.latitude
+        d_lon = destination.longitude
+
+        pointB = (d_lat, d_lon)
+
+        # Distance calculation
+
+        distance = round(geodesic(pointA, pointB).km, 2)
+
+        instance.location = location
+        instance.distance = distance
+        instance.save()
 
     context = {
         'distance': obj,
