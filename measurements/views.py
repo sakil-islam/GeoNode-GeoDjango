c0c136011955e313ffe7d63f5bfa7feb4c253e0e
Mehedi Hasan <mehedi.cse14bu@gmail.com>
2021-10-29 09:39:38 +0600

    Map location are centralized

--- measurements/views.py
+++ measurements/views.py
@@ -3,7 +3,9 @@ from .models import Measurement
 from .forms import MeasurementModelForm
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
-from .utils import get_geo
+import folium
+from .utils import get_geo, get_center_coordinates, get_zoom
+
 # Create your views here.
 
 
@@ -22,6 +24,14 @@ def calculate_distance_view(request):
     l_lon = lon
     pointA = (l_lat, l_lon)
 
+    # initial folium map
+    m = folium.Map(width=600, height=400,
+                   location=get_center_coordinates(l_lat, l_lon), zoom_start=1)
+
+    # location marker
+    folium.Marker([l_lat, l_lon], tooltip='click here for more',
+                  popup=city['city'], icon=folium.Icon(color='purple')).add_to(m)
+
     if form.is_valid():
         instance = form.save(commit=False)
         destination_ = form.cleaned_data['destination']
@@ -38,13 +48,27 @@ def calculate_distance_view(request):
 
         distance = round(geodesic(pointA, pointB).km, 2)
 
+        # folium Map modification
+        m = folium.Map(width=600, height=400, location=get_center_coordinates(
+            l_lat, l_lon, d_lat, d_lon), zoom_start=get_zoom(distance))
+        # location marker
+        folium.Marker([l_lat, l_lon], tooltip='click here for more',
+                      popup=city['city'], icon=folium.Icon(color='purple')).add_to(m)
+
+        # destination marker
+        folium.Marker([d_lat, d_lon], tooltip='click here for more',
+                      popup=destination, icon=folium.Icon(color='red', icon='cloud')).add_to(m)
+
         instance.location = location
         instance.distance = distance
         instance.save()
 
+    m = m._repr_html_()
+
     context = {
         'distance': obj,
         'form': form,
+        'map': m,
     }
 
     return render(request, 'measurements/main.html', context)
